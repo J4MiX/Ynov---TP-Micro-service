cf2556397ba79c47f16ca0d1c309ac7a3c2953e5
Jamison CHARDRON <jami.chardron@gmail.com>
2021-01-12 11:07:53 +0100

    Partie 6

--- microservice-currencyconversionservice/src/main/java/com/tactfactory/microservice/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ microservice-currencyconversionservice/src/main/java/com/tactfactory/microservice/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	CurrencyConversion convertCurrencyFeign(@PathVariable("from") String from, @PathVariable("to") String to);
 }
